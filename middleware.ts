efb01e101187212d9763430c73e437e5859ff511
Nadun Malwenna <107237482+HewageNKM@users.noreply.github.com>
2025-10-24 15:23:46 +0530

    middleware configured

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,26 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
-export function middleware(req:Request) {
-    const origin = req.headers.get('origin');
+export function middleware(req: Request) {
+  const origin = req.headers.get("origin");
 
-  /*  if (!origin) {
-        // No Origin header indicates a same-origin request
-        console.log('Allowed: Same-origin request');
-        return NextResponse.next(); // Allow the request
-    }
+  if (!origin) {
+    // No Origin header indicates a same-origin request
+    console.log("Allowed: Same-origin request");
+    return NextResponse.next(); // Allow the request
+  }
 
-    if (origin === 'https://pos.neverbe.lk') {
-        // Cross-origin request from allowed origin
-        console.log(`Allowed: Cross-origin request from ${origin}`);
-        return NextResponse.next(); // Allow the request
-    }
+  if (origin === "https://pos.neverbe.lk") {
+    // Cross-origin request from allowed origin
+    console.log(`Allowed: Cross-origin request from ${origin}`);
+    return NextResponse.next(); // Allow the request
+  }
 
-    // Block requests from other origins
-    console.log(`Blocked: Request from unknown origin: ${origin}`);
-    return new NextResponse('Forbidden', { status: 403 });*/
-    return NextResponse.next();
+  // Block requests from other origins
+  console.log(`Blocked: Request from unknown origin: ${origin}`);
+  return new NextResponse("Forbidden", { status: 403 });
 }
 
 // Apply the middleware to all API routes
 export const config = {
-    matcher: '/api/:path*',
+  matcher: "/api/:path*",
 };
