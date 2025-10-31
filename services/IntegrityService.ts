513d73cece5455bca8a7de0f768bfeb6a8a5310e
Nadun Malwenna <107237482+HewageNKM@users.noreply.github.com>
2025-10-31 10:47:00 +0530

    Improve hashing security

--- services/IntegrityService.ts
+++ services/IntegrityService.ts
@@ -3,10 +3,13 @@ import crypto from "crypto";
 import { FieldValue } from "firebase-admin/firestore";
 import stringify from "json-stable-stringify";
 
+const HASH_SECRET = process.env.HASH_SECRET;
+
 const generateDocumentHash = (docData: any) => {
   const dataToHash = { ...docData };
   const canonicalString = stringify(dataToHash);
-  return crypto.createHash("sha256").update(canonicalString).digest("hex");
+  const hashingString = `${canonicalString}${HASH_SECRET}`;
+  return crypto.createHash("sha256").update(hashingString).digest("hex");
 };
 
 export const updateOrAddOrderHash = async (data: any) => {
